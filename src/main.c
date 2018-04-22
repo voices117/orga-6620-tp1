1b707d1863982e6e9d025b927c89a9379d25ca30
MauroFab <maurotoscano2@gmail.com>
2018-04-22 20:34:01 -0300

    Funcionando con las matrices dadas, falta validar que la cantidad de columnas y filas sean validas

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "matrix.h"
 
 struct args {
 
@@ -95,30 +96,30 @@ static void _arg_parse(struct args* args,int argc, const char **argv) {
     }
 }
 
-void abrir_archivos(struct args args, FILE* archivo_entrada, FILE* archivo_salida){
-    archivo_entrada = fopen(args.path_entrada, "r");
-    if (archivo_entrada == 0) {
+void abrir_archivos(struct args args, FILE** archivo_entrada, FILE** archivo_salida){
+    *archivo_entrada = fopen(args.path_entrada, "r");
+    if (*archivo_entrada == 0) {
        perror("Input file error");
     }
     
     if(args.usa_std_out)
-        archivo_salida = stdout;
+        *archivo_salida = stdout;
     else{
-        archivo_salida = fopen(args.path_salida, "w");
-        if (archivo_salida == 0) {
+        *archivo_salida = fopen(args.path_salida, "w");
+        if (*archivo_salida == 0) {
             perror("Output file error");
         }
     }
     
     //Si alguno de los dos fallo cancelo lo hecho y salgo
-    if(archivo_entrada == 0 || archivo_salida == 0){
+    if(*archivo_entrada == 0 || *archivo_salida == 0){
         
-        if(archivo_entrada != 0){
-            fclose(archivo_entrada);
+        if(*archivo_entrada != 0){
+            fclose(*archivo_entrada);
         }
         
-        if(archivo_salida != 0){
-            fclose(archivo_salida);
+        if(*archivo_salida != 0){
+            fclose(*archivo_salida);
             remove(args.path_salida);
         }
         
@@ -128,10 +129,54 @@ void abrir_archivos(struct args args, FILE* archivo_entrada, FILE* archivo_salid
 
 int main(int argc, const char *argv[]){
     struct args args;
-    _arg_parse(&args,argc, argv);
     FILE* archivo_entrada;
     FILE* archivo_salida;
-    abrir_archivos(args, archivo_entrada, archivo_salida);
+    unsigned int cantidad_de_filas;
+    unsigned int cantidad_de_columnas;
+    unsigned int cantidad_de_filas_traspuesta;
+    unsigned int cantidad_de_columnas_traspuesta;
+    long long int* matriz_entrada;
+    long long int* matriz_salida;
+    int i = 0;
+    char* direccion_caracter_no_numerico = NULL;
+    char string_cargado[50];
+    long long int numero_cargado;
+    _arg_parse(&args,argc, argv);
     
+    abrir_archivos(args, &archivo_entrada, &archivo_salida);
 
+    fscanf(archivo_entrada,"%u %u\n", &cantidad_de_filas, &cantidad_de_columnas );
+    matriz_entrada = malloc(sizeof(long long int) * cantidad_de_filas * cantidad_de_columnas);
+    matriz_salida = malloc(sizeof(long long int) * cantidad_de_filas * cantidad_de_columnas);
+    
+    //Cargo la matriz de entrada
+    while(fscanf(archivo_entrada, "%s ", string_cargado) != EOF){
+        
+        numero_cargado = strtoll(string_cargado, &direccion_caracter_no_numerico, 0); //Esta en base 10;
+        
+        if(*direccion_caracter_no_numerico != '\0'){
+            fprintf( stderr, "Input file doesn't have a valid format\n"); 
+            exit(1);
+        }
+        
+        matriz_entrada[i] = numero_cargado;
+        i++;
+        
+    }
+    
+    trasponer(cantidad_de_filas, cantidad_de_columnas, matriz_entrada, matriz_salida);
+    cantidad_de_columnas_traspuesta = cantidad_de_filas;
+    cantidad_de_filas_traspuesta = cantidad_de_columnas;
+    
+    fprintf(archivo_salida, "%u %u\n", cantidad_de_filas_traspuesta, cantidad_de_columnas_traspuesta );
+    for (unsigned i = 0; i < cantidad_de_filas_traspuesta; i++) {
+        for (unsigned j = 0; j < cantidad_de_columnas_traspuesta; j++) {
+            fprintf(archivo_salida, "%lld ",matriz_salida[i * cantidad_de_columnas_traspuesta + j]);
+        } 
+        fputc('\n', archivo_salida);
+    }
+    
+    free(matriz_entrada);
+    free(matriz_salida);
+    
 }
